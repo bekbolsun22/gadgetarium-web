cfa1e9c9f2d00693089de126535621a227eb4f88
Elizar Aitbek <elizaraitbek@MacBook-Pro-Elizar.local>
2023-01-17 15:48:44 +0600

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import React from "react";
-import { Box } from "@mui/material";
 
 function App() {
-    return (
-        <div className="App">
-            hello
-            <Box
-                sx={{
-                    width: "300px",
-                    height: "300px",
-                    bgcolor: "primary.main",
-                }}
-            />
-        </div>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
