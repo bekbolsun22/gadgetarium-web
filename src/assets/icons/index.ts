fd95fae54065b347d0ae6e20d7536565f02f28ab
Bektur12 <kanybekovbektur22@gmail.com>
2023-01-18 15:31:44 +0600

    add checkbox

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,2 @@
+export { ReactComponent as CheckboxActiveIcon } from "../icons/checkbox-active.svg";
+export { ReactComponent as CheckboxDefaultIcon } from "../icons/checkbox-default.svg";
--- src/components/UI/Checkbox.tsx
+++ src/components/UI/Checkbox.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Checkbox as MuiCheckbox } from "@mui/material";
+import {
+  CheckboxActiveIcon,
+  CheckboxDefaultIcon,
+} from "../../assets/icons/index";
 
-function Checkbox() {
-  return <div>Checkbox</div>;
+interface CheckboxProps {
+  checked: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange }) => {
+  return (
+    <MuiCheckbox
+      checked={checked}
+      onChange={onChange}
+      icon={<CheckboxDefaultIcon />}
+      checkedIcon={<CheckboxActiveIcon />}
+    />
+  );
+};
 export default Checkbox;
