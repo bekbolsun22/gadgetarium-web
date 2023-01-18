95dae3b6e834fe1fe7414a0c00cd68547fef5e39
Bektur12 <kanybekovbektur22@gmail.com>
2023-01-18 13:16:44 +0600

    save changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Inputform from "./components/UI/Input";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Inputform props="input" />
+      <Inputform props="button" />
+      <Inputform props="search" />
+      
+    </div>
+  );
 }
 
 export default App;
