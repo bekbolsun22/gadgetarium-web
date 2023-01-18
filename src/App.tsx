1831f9927b1bb7063a55fed93583cdd22f9a375e
Bektur12 <kanybekovbektur22@gmail.com>
2023-01-18 14:39:36 +0600

    add checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Checkbox from "./components/UI/Checkbox";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Checkbox label="hello" />
+    </div>
+  );
 }
 
 export default App;
